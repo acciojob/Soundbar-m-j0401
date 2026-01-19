71dcbb0a4ad39b24b3e41ce336bda65fbf594b5a
m-j0401 <mehul.jain2206@gmail.com>
2026-01-19 18:48:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+let btn=document.querySelectorAll("button");
+
+for(let b of btn)
+{
+	b.addEventListener("click",playSound)	
+}
+
+function playSound(e) {
+	let t=e.target.id.slice(3)-1;
+	let arr=[1,2,3,4,5,6];
+	
+	
+	
+}
